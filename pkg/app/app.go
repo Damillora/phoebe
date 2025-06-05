f787740440c7128e0260b1b15c358931d1db57a6
Damillora <Damillora@users.noreply.github.com>
2025-06-06 04:01:49 +0700

    fix: specially reserve index.html

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -38,6 +38,9 @@ func Start() {
 		//c.String(http.StatusOK, "AAA")
 		c.FileFromFS("./index.html", http.FS(webFS))
 	})
+	g.GET("/index.html", func(c *gin.Context) {
+		c.FileFromFS("./index.html", http.FS(webFS))
+	})
 	g.StaticFS("/_app", http.FS(webAssets))
 	g.Static("/data", config.CurrentConfig.DataDirectory)
 	g.Use(cors.Default())
