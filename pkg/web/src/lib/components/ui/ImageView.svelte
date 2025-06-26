a015234ebab1b900bf45bd0d680406ff77599160
Damillora <developer@damillora.com>
2025-06-27 03:54:59 +0700

    fix: use API endpoint to get images as well

--- pkg/web/src/lib/components/ui/ImageView.svelte
+++ pkg/web/src/lib/components/ui/ImageView.svelte
@@ -1,33 +1,36 @@
 <script>
+    import { apiUrl } from "$lib/stores";
     import { onMount } from "svelte";
+    import { get } from "svelte/store";
 
     let { alt, src } = $props();
     let loading = $state(false);
-	let failed = $state(false);
+    let failed = $state(false);
 
-	onMount(() => {
-			const img = new Image();
-			img.src = src;
-			loading = true;
+    let url = get(apiUrl);
 
-			img.onload = () => {
-					loading = false;
-					failed = false;
-			};
-			img.onerror = () => {
-					loading = true;
-					failed = true;
-			};
-	})
+    onMount(() => {
+        const img = new Image();
+        img.src = src;
+        loading = true;
+
+        img.onload = () => {
+            loading = false;
+            failed = false;
+        };
+        img.onerror = () => {
+            loading = true;
+            failed = true;
+        };
+    });
 </script>
 
 {#if !failed}
-
-<figure class:is-skeleton="{loading}">
-	<img {src} {alt} />
-</figure>
+    <figure class:is-skeleton={loading}>
+        <img src={url + src} {alt} />
+    </figure>
 {:else}
-<div class="notification is-danger is-light">
-	There was an error loading this image.
-</div>
+    <div class="notification is-danger is-light">
+        There was an error loading this image.
+    </div>
 {/if}
