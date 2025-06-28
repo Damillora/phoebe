d313fe0f15ed49954a738fa5452947fc3fd2ed67
Damillora <developer@damillora.com>
2025-06-29 05:31:23 +0700

    feat: migrate api calling to fetch

--- pkg/web/src/lib/api.ts
+++ pkg/web/src/lib/api.ts
@@ -3,6 +3,7 @@ import { browser } from "$app/environment";
 import axios from "axios";
 import { isDesktop } from "./desktop-check.js";
 import axiosTauriApiAdapter from "axios-tauri-api-adapter";
+import { fetch as tauriFetch } from "@tauri-apps/plugin-http";
 
 let url = (browser && window.location.origin) || "";
 apiUrl.subscribe((value) => {
@@ -12,79 +13,78 @@ let current_token;
 token.subscribe((value) => {
   current_token = value;
 });
-
-let client = isDesktop()
-  ? axios.create({
-      adapter: axiosTauriApiAdapter,
-    })
-  : axios.create();
+let currentFetch = isDesktop() ? tauriFetch : fetch;
 
 export async function login({ username, password }) {
   const endpoint = url + "/api/auth/login";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "POST",
-    data: JSON.stringify({
+    body: JSON.stringify({
       username,
       password,
     }),
   });
-  token.set(response.data.token);
-  return response.data;
+  const data = await response.json();
+  token.set(data.token);
+  return data;
 }
 
 export async function register({ email, username, password }) {
   const endpoint = url + "/api/user/register";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "POST",
-    data: JSON.stringify({
+    body: JSON.stringify({
       email,
       username,
       password,
     }),
   });
-  token.set(response.data.token);
-  return response.data;
+  const data = await response.json();
+  token.set(data.token);
+  return data;
 }
 
 export async function updateToken() {
   const endpoint = url + "/api/auth/token";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "POST",
     headers: {
       Authorization: "Bearer " + current_token,
     },
   });
-  token.set(response.data.token);
-  return response.data;
+  const data = await response.json();
+  token.set(data.token);
+  return data;
 }
 
 export async function getTags() {
   const endpoint = url + "/api/tag";
-  const response = await client.get(endpoint);
-  return response.data;
+  const response = await currentFetch(endpoint);
+  const data = await response.json();
+  return data;
 }
 
 export async function getTag({ tag }) {
   const endpoint = url + "/api/tag/" + tag;
-  const response = await client.get(endpoint);
-  return response.data;
+  const response = await currentFetch(endpoint);
+  const data = await response.json();
+  return data;
 }
 
 export async function getRelatedTags({ tag }) {
   const endpoint = url + "/api/tag-related/" + tag;
-  const response = await client.get(endpoint);
-  return response.data;
+  const response = await currentFetch(endpoint);
+  const data = await response.json();
+  return data;
 }
 export async function getTagAutocomplete({ tag, positive }) {
   let endpoint = url + "/api/tag-autocomplete?tag=" + tag;
   if (positive) {
     endpoint = endpoint + "&positive=true";
   }
-  const response = await client.get(endpoint);
-  return response.data;
+  const response = await currentFetch(endpoint);
+  const data = await response.json();
+  return data;
 }
 export async function getPosts({
   page,
@@ -103,186 +103,176 @@ export async function getPosts({
     endpoint =
       url + "/api/post?tags=" + q + "&page=" + page + "&perPage=" + perPage;
   }
-  const response = await client.get(endpoint);
-  return response.data;
+  const response = await currentFetch(endpoint);
+  const data = await response.json();
+  return data;
 }
 
 export async function getPost({ id }) {
   const endpoint = url + "/api/post/" + id;
-  const response = await client(endpoint);
-  return response.data;
+  const response = await currentFetch(endpoint);
+  const data = await response.json();
+  return data;
 }
 
 export async function getPostCount() {
   const endpoint = url + "/api/post-count";
-  const response = await client(endpoint);
-  return response.data;
+  const response = await currentFetch(endpoint);
+  const data = await response.json();
+  return data;
 }
 
 export async function uploadBlob({ file }) {
   var formData = new FormData();
   formData.append("file", file);
   const endpoint = url + "/api/blob/upload";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "POST",
     headers: {
       Authorization: "Bearer " + current_token,
-      "Content-Type": "multipart/form-data",
     },
-    withCredentials: true,
-    data: formData,
+    body: formData,
   });
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 
 export async function searchBlob({ file }) {
   var formData = new FormData();
   formData.append("file", file);
   const endpoint = url + "/api/blob/search";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "POST",
     headers: {
       Authorization: "Bearer " + current_token,
-      "Content-Type": "multipart/form-data",
     },
-    withCredentials: true,
-    data: formData,
+    body: formData,
   });
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 
 export async function postCreate({ blob_id, source_url, tags }) {
   const endpoint = url + "/api/post/create";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "POST",
     headers: {
       Authorization: "Bearer " + current_token,
     },
-    withCredentials: true,
-    data: {
+    body: JSON.stringify({
       blob_id,
       source_url,
       tags,
-    },
+    }),
   });
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 
 export async function postUpdate(id, { source_url, tags }) {
   const endpoint = url + "/api/post/" + id;
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "POST",
     headers: {
       Authorization: "Bearer " + current_token,
     },
-    withCredentials: true,
-    data: {
+    body: JSON.stringify({
       source_url,
       tags,
-    },
+    }),
   });
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 
 export async function postDelete({ id }) {
   const endpoint = url + "/api/post/" + id;
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "DELETE",
     headers: {
       Authorization: "Bearer " + current_token,
     },
-    withCredentials: true,
   });
   return response.status == 200;
 }
 
 export async function getUserProfile() {
   const endpoint = url + "/api/user/profile";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "GET",
     headers: {
       Authorization: "Bearer " + current_token,
     },
-    withCredentials: true,
   });
-  console.log(response.data);
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 
 export async function updateTagNotes(id, { note }) {
   const endpoint = url + "/api/tag/" + id + "/note";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "PUT",
     headers: {
       Authorization: "Bearer " + current_token,
     },
-    withCredentials: true,
-    data: {
+    body: JSON.stringify({
       note,
-    },
+    }),
   });
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 
 export async function updateTag(id, { name, tagTypeId }) {
   const endpoint = url + "/api/tag/" + id;
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "PUT",
     headers: {
       Authorization: "Bearer " + current_token,
     },
-    withCredentials: true,
-    data: {
+    body: JSON.stringify({
       name,
       tagTypeId,
-    },
+    }),
   });
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 
 export async function updateUserProfile({ email, username }) {
   const endpoint = url + "/api/user/update";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "PUT",
     headers: {
       Authorization: "Bearer " + current_token,
     },
-    withCredentials: true,
-    data: {
+    body: JSON.stringify({
       email,
       username,
-    },
+    }),
   });
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 export async function updateUserPassword({ old_password, new_password }) {
   const endpoint = url + "/api/user/update-password";
-  const response = await client({
-    url: endpoint,
+  const response = await currentFetch(endpoint, {
     method: "PUT",
     headers: {
       Authorization: "Bearer " + current_token,
     },
-    withCredentials: true,
-    data: {
+    body: JSON.stringify({
       old_password,
       new_password,
-    },
+    }),
   });
-  return response.data;
+  const data = await response.json();
+  return data;
 }
 
 export async function getTagTypes() {
   const endpoint = url + "/api/tagtype";
-  const response = await client.get(endpoint);
-  return response.data;
+  const response = await currentFetch(endpoint);
+  const data = await response.json();
+  return data;
 }
