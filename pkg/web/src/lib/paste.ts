c65ef2cc2fcc64c8a163a68fc30dfcaa06a44eb9
Damillora <developer@damillora.com>
2025-06-30 02:44:52 +0700

    fix: clean up paste.ts

--- pkg/web/src/lib/paste.ts
+++ pkg/web/src/lib/paste.ts
@@ -1,7 +1,3 @@
-import { isDesktop } from "./desktop-check";
-import { readImage } from "@tauri-apps/plugin-clipboard-manager";
-import { invoke } from "@tauri-apps/api/core";
-
 const handlePaste = async (e?: any) => {
   if (e.clipboardData.files[0]) {
     return e.clipboardData.files[0];
