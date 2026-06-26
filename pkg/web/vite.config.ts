8cdee4dbcc19d1ee0019fdafdfc57f92ab0182e5
Damillora <damillora@damillora.com>
2026-06-26 22:13:23 +0700

    chore: update node deps

--- pkg/web/vite.config.ts
+++ pkg/web/vite.config.ts
@@ -1,7 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-static";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit({
+    adapter: adapter({
+      fallback: "app.html", // may differ from host to host
+    })
+  })],
 });
